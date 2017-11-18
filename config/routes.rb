aa8e6f7a182dd918076eff7b5433dc9f279a2353
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-18 03:31:12 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/packages'
+
+  get 'pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
