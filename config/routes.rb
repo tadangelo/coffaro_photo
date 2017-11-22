54096efaeb50ac8a012d444dae37a80be16c3b9e
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-22 04:08:07 -0600

    Updated content and integrated bootstrap gem and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/about'
+  get 'about', to: 'pages#about'
 
-  get 'pages/packages'
+  get 'packages', to: 'pages#packages'
 
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
